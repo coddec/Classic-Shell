674a486c7f377080deaa5bb13bf85ce2c4d6eb48
ge0rdi <ge0rdi@users.noreply.github.com>
2022-12-27 22:01:10 +0100

    Make sure tray buttons are properly positioned when custom start button was created
    Fixes #1232

--- Src/StartMenu/StartMenuDLL/StartMenuDLL.cpp
+++ Src/StartMenu/StartMenuDLL/StartMenuDLL.cpp
@@ -3110,6 +3110,12 @@ static void RecreateStartButton( size_t taskbarId )
 		RECT rcTask;
 		GetWindowRect(taskBar.taskBar,&rcTask);
 		PostMessage(taskBar.taskBar,WM_SIZE,SIZE_RESTORED,MAKELONG(rcTask.right-rcTask.left,rcTask.bottom-rcTask.top));
+		for (auto btn : taskBar.trayButtons)
+		{
+			RECT rc;
+			GetWindowRect(btn,&rc);
+			SetWindowPos(btn,HWND_TOP,rc.left,rc.top,0,0,SWP_NOSIZE|SWP_NOACTIVATE|SWP_NOZORDER);
+		}
 	}
 }
 
